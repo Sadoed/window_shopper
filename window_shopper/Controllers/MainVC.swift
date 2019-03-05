6289e0e4671af69434478c15d70882ecc18f5da2
Sadoed <barmoza999@gmail.com>
2019-03-05 23:29:35 +0300

    add: made a blank of app

---
+++ window_shopper/Controllers/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window_shopper
+//
+//  Created by more on 05/03/2019.
+//  Copyright © 2019 Archailect. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ window_shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,33 @@
+//
+//  CurrencyTextField.swift
+//  window_shopper
+//
+//  Created by more on 05/03/2019.
+//  Copyright © 2019 Archailect. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+     
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: placeholder!,
+                                           attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+    }
+        
+        
+     
+
+}
