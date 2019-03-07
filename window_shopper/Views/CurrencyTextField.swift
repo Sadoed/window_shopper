dd50aca18fe22e434fc5e15990735bf7dc20d944
Sadoed <barmoza999@gmail.com>
2019-03-08 00:55:41 +0300

    add: -symbol conects with device local region

--- window_shopper/Views/CurrencyTextField.swift
+++ window_shopper/Views/CurrencyTextField.swift
@@ -11,6 +11,22 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
 
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2,
+                                                  width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.85292644, green: 0.85292644, blue: 0.85292644, alpha: 0.7971494932)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.clipsToBounds = true
+        currencyLabel.layer.cornerRadius = 5.0
+        let formater = NumberFormatter()
+        formater.numberStyle = .currency
+        formater.locale = .current
+        currencyLabel.text = formater.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -26,6 +42,8 @@ class CurrencyTextField: UITextField {
     layer.cornerRadius = 5.0
     textAlignment = .center
     
+    clipsToBounds = true
+        
     if let p = placeholder {
         let place = NSAttributedString(string: placeholder!,
                                        attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
